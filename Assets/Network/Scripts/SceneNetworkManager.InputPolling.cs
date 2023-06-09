a793aea6c3b198dd375fdffbde26034d2b33c7b3
favas89 <itisfavas89@gmail.com>
2023-06-09 12:51:54 +0530

    Fixed project issues

---
+++ Assets/Network/Scripts/SceneNetworkManager.InputPolling.cs
@@ -0,0 +1,18 @@
+﻿using Fusion;
+using Player;
+using UnityEngine;
+
+namespace Network
+{
+    public partial class SceneNetworkManager
+    {
+        private PlayerInput _myInput;
+
+        private void Update()
+        {
+            _myInput.NetworkButtons.Set(PlayerInputMapper.HandBrake, Input.GetKey(KeyCode.Space));
+            _myInput.Acceleration = Input.GetAxis("Vertical");
+            _myInput.SteeringValue = Input.GetAxis("Horizontal");
+        }
+    }
+}
